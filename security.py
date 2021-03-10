86008be0f2b03fe370b654097bb5850b047289d5
ronaldoapsilva <ronaldoapsilva_@hotmail.com>
2021-03-10 01:20:00 -0300

    74. PUT to create or update Items

--- security.py
+++ security.py
@@ -1,3 +1,4 @@
+from werkzeug.security import safe_str_cmp
 from user import User
 
 users = [
@@ -8,8 +9,8 @@ username_mapping = {u.username: u for u in users}
 userid_mapping = {u.id: u for u in users}
 
 def authenticate(username, password):
-    user = username.get(username, None)
-    if user and user.password == password:
+    user = username_mapping.get(username, None)
+    if user and safe_str_cmp(user.password, password):
         return user
 
 def identity(payload):
