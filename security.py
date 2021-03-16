5baee3345a3785dd394bd9a083ca5596e208234a
Ronaldo Aparecido Da Silva <ronaldos@br.ibm.com>
2021-03-16 00:50:59 -0300

    aula 80

--- security.py
+++ security.py
@@ -1,19 +1,12 @@
 from werkzeug.security import safe_str_cmp
 from user import User
 
-users = [
-    User(1, 'bob', 'asdf')
-]
-
-username_mapping = {u.username: u for u in users}
-userid_mapping = {u.id: u for u in users}
-
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
+    user = User.find_by_username(username, None)
     if user and safe_str_cmp(user.password, password):
         return user
 
 def identity(payload):
     #here 'identity' is the secrekey
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id, None)
