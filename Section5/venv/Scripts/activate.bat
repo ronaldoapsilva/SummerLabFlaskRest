ee56d84a73349b6ca75e08defff2905c61709228
Ronaldo Aparecido Da Silva <ronaldos@br.ibm.com>
2021-03-20 01:55:36 -0300

    88. Retrieving many items from the database

--- Section5/venv/Scripts/activate.bat
+++ Section5/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\RONALDOAPARECIDODASI\Desktop\SummerLabFlaskRest\Section5\venv"
+set "VIRTUAL_ENV=C:\Users\RONALDOAPARECIDODASI\Documents\MeusProjetos\FlaskTutorials\SummerLabFlaskRest\Section5\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
