1e66df1ebf12b1bea9fa933e9bc8e8c3c6ce2c50
Ronaldo Aparecido Da Silva <ronaldos@br.ibm.com>
2021-03-23 23:10:11 -0300

    93. Creating User and Item models

---
+++ Section6/code/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+
+app = Flask(__name__)
+app.secret_key = 'jose'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity) #/auth
+        
+api.add_resource(Item, '/item/<string:name>') #http://127.0.0.1:5000/Item/Rolf
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True) #debug it will display a nice page when got a error
